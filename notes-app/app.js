2e05cd2720ba16a1e5656782c9c4aa313e05743e
Enzo Parodi <enzo.parodi@syzygy.de>
2019-08-13 10:48:06 +0200

    Lesson 17 completed

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,61 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const msg = getNotes();
+//Customize yargs version
+yargs.version('1.1.0');
 
-console.log(msg);
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    console.log('Title: ' + argv.title);
+    console.log('Body: ' + argv.body);
+    console.log('argv', argv);
+  }
+});
 
-const invertedSuccess = chalk.green.bold.inverse;
-console.log(invertedSuccess('Success!'));
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function() {
+    console.log('Removing the note!');
+  }
+});
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List the notes',
+  handler: function() {
+    console.log('Listing out all notes!');
+  }
+});
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Reading a note!',
+  handler: function() {
+    console.log('Reading a note!');
+  }
+});
+
+// add, remove, read, list
+
+yargs.parse();
+// console.log(yargs.argv);
