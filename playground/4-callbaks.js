3840e93a5596b20991db8a70f65b900272339946
Enzo Parodi <enzo.parodi@syzygy.de>
2019-08-16 17:52:36 +0200

    Half of lesson 35

---
+++ playground/4-callbaks.js
@@ -0,0 +1,19 @@
+setTimeout(() => {
+  console.log('Two seconds are up');
+}, 2000);
+
+const names = ['Enzo', 'Jen', 'Jes'];
+const shortNames = names.filter(name => {
+  return name.length <= 3;
+});
+
+const geocode = (address, callback) => {
+  const data = {
+    latitude: 0,
+    longitude: 0
+  };
+
+  return data
+};
+
+geocode('Philadelfia', )
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,35 @@
 const request = require('request');
 
-const url =
-  'https://api.darksky.net/forecast/996aab7438b40786a2c1d91b14f00706/50.2005,8.5804?units=ca&slang=es';
+// const url =
+//   'https://api.darksky.net/forecast/996aab7438b40786a2c1d91b14f00706/50.2005,8.5804?units=ca&slang=es';
+
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log('Unable to connect to weather service!');
+//   } else if (response.body.error) {
+//     console.log('Unable to find location');
+//   } else {
+//     const temperature = response.body.currently.temperature;
+//     const precipProbability = response.body.currently.precipProbability;
+//     console.log(
+//       `${
+//         response.body.daily.data[0].summary
+//       } It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain`
+//     );
+//   }
+// });
 
 const geocodeURL =
   'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZW56by1wYXJvZGkiLCJhIjoiY2p6Y2N3OHBwMDRmYTNnbWcydGhmdGlmbSJ9.pFJo4fFLNaBTcZisSmz0YA&limit=1';
 
-request({ url: url, json: true }, (error, response) => {
-  const temperature = response.body.currently.temperature;
-  const precipProbability = response.body.currently.precipProbability;
-
-  //   console.log(
-  //     `${
-  //       response.body.daily.data[0].summary
-  //     } It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain`
-  //   );
-});
-
 request({ url: geocodeURL, json: true }, (error, response) => {
-  const longitude = response.body.features[0].center[0];
-  const latitude = response.body.features[0].center[1];
-
-  console.log(`latitude: ${latitude}, longitude: ${longitude}`);
+  if (error) {
+    console.log('Unable to connect to location services!');
+  } else if (!response.body.features.length) {
+    console.log('hisbd');
+  } else {
+    const longitude = response.body.features[0].center[0];
+    const latitude = response.body.features[0].center[1];
+    console.log(`latitude: ${latitude}, longitude: ${longitude}`);
+  }
 });
